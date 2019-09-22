07bdd9e45cd9aeabc6cc8f9d3bdb3c60d6c2d408
jmash <jaredmashcraft@gmail.com>
2019-09-22 17:20:34 -0400

    :construction: Container for questions now in App

--- src/Presentational/App.js
+++ src/Presentational/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import QuestionContainer from '../Container/QuestionContainer';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <p>Stuff will be here.</p>
+      <QuestionContainer></QuestionContainer>
     </div>
   );
 }
