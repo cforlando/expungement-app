f0181548f6ae4db5a4285d30c9fc60f3411e0140
jmash <jaredmashcraft@gmail.com>
2019-09-22 17:25:17 -0400

    :building_construction: Adds error component

---
+++ src/Presentational/QuestionError.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Jumbotron from 'react-bootstrap/Jumbotron';
+
+const QuestionError = () => {
+    return (
+        <Jumbotron>
+            <h1>Error connecting to form data</h1>
+        </Jumbotron>
+    );
+};
+
+export default QuestionError;
