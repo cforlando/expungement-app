b5e33bcb62b5e08fb02c056eeeb37b92851589b0
jmash <jaredmashcraft@gmail.com>
2019-10-05 23:22:11 -0400

    :construction: Questions now present correctly

--- src/Presentational/QuestionPresentational.js
+++ src/Presentational/QuestionPresentational.js
@@ -2,8 +2,32 @@ import React from 'react';
 import Jumbotron from 'react-bootstrap/Jumbotron';
 
 const QuestionPresentational = (props) => {
+    // form type will be dependent on the object being passed down from props
+
+    const choices = [];
+    if(props.choices) { // choices have to actually exist to be populated
+        for(let el in props.choices) {
+            choices.push(
+                <div key={`ques_${el}`} >
+                    <input type="radio" key={el} name={props.id} value={props.choices[el]}></input>
+                    <label>{ props.choices[el] }</label>
+                </div>
+            );
+        }
+    }
+
+    let QuestionForm = 
+        <form>
+            { props.question }
+            <div id="choiceSelect">
+                { choices }
+            </div>
+        </form>
+        
     return (
-        <Jumbotron><h1>{props.question}</h1></Jumbotron>
+        <Jumbotron>
+            { QuestionForm }
+        </Jumbotron>
     )
 }
 
